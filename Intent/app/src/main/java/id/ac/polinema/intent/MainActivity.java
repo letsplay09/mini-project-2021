00e2647ed14bd2bb7ff9995acd7544a5fcdc1fc7
HernandaCandraP <hernandacp@gmail.com>
2019-10-06 22:52:35 +0700

    add fungtion explicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -2,7 +2,9 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleExplicitIntent(View view) {
+        Intent intent =  new Intent(this, ExplicitIntentActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleExit(View view) {
+        finish();
     }
 
 }
