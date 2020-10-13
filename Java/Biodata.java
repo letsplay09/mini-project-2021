3088c71ada635e6ec8d342971072b61e30d80093
Muhammad Alfarizi <malfarizi259@gmail.com>
2020-10-13 10:26:15 +0700

    Menambahkan Code

--- Java/Biodata.java
+++ Java/Biodata.java
@@ -8,16 +8,19 @@ class Biodata
 		Scanner scInt = new Scanner (System.in);
 		Scanner scString = new Scanner (System.in) ;
 
-		String nama, noHp;
+		String nama, noHp, ttl;
 		int usia, tinggiBadan;
 
-		System.out.print("Masukan nama :") ;
+		System.out.print("Masukan Nama Lengkap :") ;
 		nama = scString.nextLine();
 
-		System.out.print("Masukkan no Hp :") ;
+		System.out.print("Masukkan No. Telepon :") ;
 		noHp = scString.nextLine();
 
-		System.out.print("masukkan usia :") ;
+		System.out.print("Masukkan Tempat Tanggal Lahir :") ;
+		ttl = scString.nextLine();
+
+		System.out.print("masukkan Usia :") ;
 		usia = scInt.nextInt();
 
 		System.out.print("Tinggi badan: ") ;
@@ -26,6 +29,7 @@ class Biodata
 		System.out.println("Boidata") ;
 		System.out.println("Nama:" + nama ) ;
 		System.out.println("No Hp:" + noHp ) ;
+		System.out.println("TTL:" + ttl ) ;
 		System.out.println("Usia:" + usia ) ;
 		System.out.println("Tinggi Badan :" + tinggiBadan ) ;
 
